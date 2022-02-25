ccb0c8ed169662134cc750cf2c8fe37344cdfdc9
learning2html <honganhle0a@gmail.com>
2022-02-25 12:50:01 +0700

    fix the main function

--- src/com/anhlh/mathutil/core/MathUtil.java
+++ src/com/anhlh/mathutil/core/MathUtil.java
@@ -28,7 +28,7 @@ public class MathUtil {
         if (n == 0 || n == 1)
             return 1;
         //song sot den day, sure n = 2...20
-        long product = 1000000; //ket qua trung gian
+        long product = 1; //ket qua trung gian
         for (int i = 2; i <= n; i++) 
             product *= i;
         return product;
